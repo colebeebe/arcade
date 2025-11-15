70e3ff24c7e6566410d6b983655c7aba70981f68
colebeebe <bee14003@byui.edu>
2025-11-15 08:51:00 -0500

    Player movement and gravity now functional

--- LunarLander/Scripts/player.gd
+++ LunarLander/Scripts/player.gd
@@ -2,19 +2,22 @@ extends CharacterBody2D
 
 @export var line_weight: float = 1.0
 
-const ROTATE_SPEED := 180.0
-const ACCELERATION := 15.0
+const ROTATE_SPEED := 50.0
+const ACCELERATION := 0.3
 
-var vel := Vector2.ZERO
+var vel := Vector2(30.0, 0.0)
+var is_on_ground := false
 
 func _ready() -> void:
 	for graphic in $Graphics.get_children():
 		graphic.width = line_weight
+	rotate(-PI / 2)
 
 
 func _physics_process(delta: float) -> void:
-	handle_input(delta)
-	update_position(delta)
+	if not is_on_ground:
+		handle_input(delta)
+		update_position(delta)
 
 
 func handle_input(delta: float) -> void:
@@ -31,4 +34,5 @@ func handle_input(delta: float) -> void:
 
 
 func update_position(delta: float) -> void:
+	vel += Vector2(0.0, 0.18)
 	position += vel * delta
