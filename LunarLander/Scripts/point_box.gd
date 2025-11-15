f07938e219e88bfda50f126ba01a1684afb60568
colebeebe <bee14003@byui.edu>
2025-11-15 12:14:26 -0500

    Lunar lander displays messages and resets properly; zero fuel triggers game over

--- LunarLander/Scripts/point_box.gd
+++ LunarLander/Scripts/point_box.gd
@@ -8,15 +8,22 @@ func _on_body_entered(body: Node2D) -> void:
 		body.kill_animation()
 		var root = get_tree().current_scene.get_node("LunarLander")
 		var message_box = root.get_node("CanvasLayer").get_node("Message")
-		if abs(body.vel.y * 1.95) > 16 && abs(body.vel.x * 1.95) > 3:
+		if abs(body.vel.y * 1.95) > 16 or abs(body.vel.x * 1.95) > 3:
 			body.get_node("Graphics").visible = false
 			message_box.text = "That was a pretty rough landing!\nYour vessel was destroyed!"
-			var fuel_loss = randi_range(100, 250)
-			message_box.text += "\n\n%d units of fuel lost!" % [fuel_loss]
-			root.get_node("Player").fuel -= fuel_loss
+			if body.fuel > 0:
+				var fuel_loss = randi_range(100, 250)
+				if fuel_loss > body.fuel:
+					fuel_loss = body.fuel
+				message_box.text += "\n\n%d units of fuel lost!" % [fuel_loss]
+				body.fuel -= fuel_loss
 		else:
 			var rating = body.fuel * 5 * point_multiplier
 			root.score += rating
 			message_box.text = "A beautiful landing!\n\nRating: %d" % [rating]
-		root.reset_game()
+		
+		if body.fuel == 0:
+			message_box.text += "\n\nYou are out of fuel.\n\nGAME OVER"
+		else:
+			root.reset_game()
 			
--- LunarLander/terrain.gd
+++ LunarLander/terrain.gd
@@ -8,7 +8,15 @@ func _on_body_entered(body) -> void:
 		var root = get_tree().current_scene.get_node("LunarLander")
 		var message_box = root.get_node("CanvasLayer").get_node("Message")
 		message_box.text = "You caused irreparable damage to a\nmultimillion dollar piece of equipment."
-		var fuel_loss = randi_range(100, 250)
-		message_box.text += "\n\n%d units of fuel lost." % [fuel_loss]
-		body.fuel -= fuel_loss
-		root.reset_game()
+		if body.fuel > 0:
+			var fuel_loss = randi_range(100, 250)
+			if fuel_loss > body.fuel:
+				fuel_loss = body.fuel
+			message_box.text += "\n\n%d units of fuel lost." % [fuel_loss]
+			body.fuel -= fuel_loss
+			if body.fuel == 0:
+				message_box.text += "\n\nYou are out of fuel.\n\nGAME OVER"
+			else:
+				root.reset_game()
+		else: 
+			message_box.text += "\n\nYou are out of fuel.\n\nGAME OVER"
